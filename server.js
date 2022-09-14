adb5cd71a02f05f2ae30070672d18f0035da0282
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-09-14 18:03:25 -0300

    Change port to deploy with Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3030 //Por conta do deploy no Heroku
 
 const routes = require('./src/Routes/Routes')
 
@@ -7,6 +8,7 @@ app.use(routes)
 
 //Use gate 80 because deploy aplication on web
 //Change to another gate to test in localhost
-app.listen(3033, () => {
+app.listen(port, () => {
     console.log('Server working...')
+    console.log(`http://localhost:${port}`)
 })
