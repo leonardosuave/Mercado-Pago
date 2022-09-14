387f99f63b01b3d1ba6c32cb429fa626b15466d4
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-09-14 01:58:20 -0300

    Update pagamentoController.js

--- src/Controllers/pagamentoController.js
+++ src/Controllers/pagamentoController.js
@@ -1,11 +1,4 @@
-const MercadoPago = require('mercadopago')
 const PayModel = require('../Models/PagamentoModel')
-
-//Integração e Configuração Mercado pago SDK
-MercadoPago.configure({
-    sandbox: true, //True porque está em desenvolvimento
-    access_token: 'TEST-4721771703735817-091315-60deb4633928ddca44934b5cbabb2e1e-104451160'
-})
 
 exports.index = async (req, res) => {
 
