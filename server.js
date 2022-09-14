26aaa0668467efc217b323eddfc4eb772acd5c57
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-09-14 04:03:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(routes)
 
 //Use gate 80 because deploy aplication on web
 //Change to another gate to test in localhost
-app.listen(80, () => {
+app.listen(3033, () => {
     console.log('Server working...')
 })
